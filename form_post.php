74feea0a535a66eed80fa24cbe613d325a589b7d
celine <celinecalenge@gmail.com>
2016-09-27 16:20:42 +0200

    uploads chgt de nom de fichier

--- form_post.php
+++ form_post.php
@@ -1,6 +1,21 @@
 <?php
 
-// tester si le fichier phot a été envoyé et s'il n'y a pas d'erreur
+include'connect.php';
+// tester si le nom et le prénom ont été entrés
+
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+
+if (!isset($nom) || empty($nom)) {
+    echo "Veuillez entrer un nom !<br/>";
+}
+
+if (!isset($prenom) || empty($prenom)) {
+  echo "Veuillez entrer un prénom !<br/>";
+}
+
+
+// tester si le fichier photo a été envoyé et s'il n'y a pas d'erreur
 if (isset($_FILES['photo']) AND $_FILES['photo']['error'] == 0) {
   // tester le poids du fichier
   if ($_FILES['photo']['size'] <= 1000000)
@@ -13,11 +28,31 @@ if (isset($_FILES['photo']) AND $_FILES['photo']['error'] == 0) {
     $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
     if (in_array($extension_upload, $extensions_autorisees))
     {
+
+          // on récupère l'extension du fichier uploadé
+          $extension = strtolower(substr(strrchr($_FILES['photo']['name'], '.'),1));
+
+          // on génère un nom aléatoire pour le nom de l'image et on ajoute l'extension
+
+          $photo = md5(uniqid(rand(), true)).".".$extension;
+
           // Valider le fichier et le stocker définitivement
-          move_uploaded_file($_FILES['photo']['tmp_name'],'uploads/'.basename($_FILES['photo']['name']));
+          $resultat =
+          move_uploaded_file($_FILES['photo']['tmp_name'],'uploads/'.$photo);
           echo "L'envoi a bien été effectué !";
     }
   }
+}
+
+else {
+  $req=$bdd->prepare('INSERT INTO trombi(nom, prenom,photo,date_insertion)
+  VALUES (:nom, :prenom, :photo, NOW())');
+  $req->execute (array(
+    'nom'=>$nom,
+    'prenom'=>$prenom,
+    'photo'=>$photo
+  ));
+  $req->closeCursor();
 }
 
  ?>
