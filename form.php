f36111a1397682fb0338c22c3bf2cd524a4467e9
celine <celinecalenge@gmail.com>
2016-09-29 14:56:29 +0200

    css

--- form.php
+++ form.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>formulaire d'ajout de photo</title>
+    <link rel="stylesheet" href="css/style.css" media="screen" title="no title"/>
   </head>
   <body>
 
@@ -10,13 +11,13 @@
 
     <form action="form_post.php" method="POST" enctype="multipart/form-data">
 
-    <label for="nom">Nom : </label><input type="text" name="nom"/><br/>
+    <label for="nom">Nom&#8239;:  </label><input type="text" name="nom"/><br/>
 
-    <label for="prenom">Prénom : </label><input type="text" name="prenom"><br/>
+    <label for="prenom">Prénom&#8239;:  </label><input type="text" name="prenom"><br/>
 
-    <label for="annee">Année d'entrée à la MOS : </label><input type="text" name="annee"><br/>
+    <label for="annee">Année d'entrée à la MOS&#8239;:  </label><input type="text" name="annee"><br/>
 
-    <label for="photo">Ajouter une trombine :
+    <label for="photo">Ajouter une trombine&#8239;: 
     </label><input type="file" name="photo"/>
 
     <input type="hidden" name="MAX_FILE_SIZE" value="1000000" /><br/>
--- galerie.php
+++ galerie.php
@@ -1,36 +1,37 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>galerie</title>
-    <link rel="stylesheet" href="css/style.css" media="screen"/>
-  </head>
-  <body>
-<h2>Année 2016</h2>
-<?php
-include 'connect.php';
+<head>
+  <meta charset="utf-8">
+  <title>galerie</title>
+  <link rel="stylesheet" href="css/style.css" media="screen"/>
+</head>
+<body>
+  <h2>Année 2016</h2>
+  <?php
+  include 'connect.php';
 
-// requête sql pour récupérer les photos
-$reponse = $bdd->query("SELECT id, prenom, nom, annee, photo FROM trombi WHERE annee = '2016' ORDER BY nom ");
-// on envoie la requête en faisant une boucle
-while ($donnees = $reponse->fetch())
-{ ?>
+  // requête sql pour récupérer les photos
+  $reponse = $bdd->query("SELECT id, prenom, nom, annee, photo FROM trombi WHERE annee = '2016' ORDER BY nom ");
+  // on envoie la requête en faisant une boucle
+  ?>
+  <div class="container">
+    <?php
+    while ($donnees = $reponse->fetch())
+    { ?>
 
+      <div class="portrait">
 
+        <img src="uploads/<?php echo $donnees['photo'];?>" />
 
-<div class="portrait">
+        <h3 class="nom"><?php echo $donnees['prenom'];?> <?php echo $donnees['nom']; ?></h3>
 
-  <img src="uploads/<?php echo $donnees['photo'];?>" />
+      </div>
 
-  <h3><?php echo $donnees['prenom'];?> <?php echo $donnees['nom']; ?></h3>
-</div>
+      <?php
+    }
+    $reponse->closeCursor();
 
-
-<?php
-}
-$reponse->closeCursor();
-
-?>
-
+    ?>
+  </div>
 </body>
 </html>
