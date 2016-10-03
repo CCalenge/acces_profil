897c59cfdac5be5af313927b983b27a6e6693772
celine <celinecalenge@gmail.com>
2016-10-03 16:55:23 +0200

    espace membre

---
+++ membres/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>login</title>
+    <link rel="stylesheet" href="../css/style.css" media="screen" title="no title"/>
+  </head>
+  <body>
+
+    <form class="" action="index.html" method="post">
+
+      <label for="login">pseudo</label><input type="text" name="login"><br/>
+
+      <label for="password">Mot de passe</label><input type="password" name="password">
+
+      <input type="submit" name="submit" value="Validez">
+
+    </form>
+
+
+
+  </body>
+</html>
