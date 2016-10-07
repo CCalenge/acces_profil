458ae222f1cb0c6cc3ba3ebfb30d6e60dfe6488f
celine <celinecalenge@gmail.com>
2016-10-07 16:32:28 +0200

    Session membres

--- membres/profil.php
+++ membres/profil.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -6,6 +8,16 @@
     <link rel="stylesheet" href="../css/style.css" media="screen" title="no title"/>
   </head>
   <body>
+
+    <!-- - un `<h1>` "Bienvenue " + login de l'utilisateur -->
+    <h1>Bienvenue <?php echo $_SESSION['pseudo']; ?></h1>
+
+        <!-- -une zone "Vos informations"
+      - Afficher "Votre profil est " + public ou privé selon le champ `public` en BDD
+      - Afficher l'avatar de l'utilisateur (rappelez-vous qu'on a renseigné une image par défaut avatar.jpg)
+      - Afficher "Prénom: " + le prénom (`firstname`) de l'utilisateur. Si celui-ci est vide en BDD, afficher "Pas de prénom indiqué"
+      - Afficher "Nom: " + le nom (`name`) de l'utilisateur. Si celui-ci est vide en BDD, afficher "Pas de nom indiqué"
+      - Afficher un lien ou bouton "Modifier" -->
 
   </body>
 </html>
